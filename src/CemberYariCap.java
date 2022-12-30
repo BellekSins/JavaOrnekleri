88ba33ae062b77be3adf69f109e368137a22f324
Fatih Bellek <fatihbellek@outlook.com.tr>
2022-12-30 20:35:48 +0300

    CemberYariCap.java Created

---
+++ src/CemberYariCap.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CemberYariCap {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Yarıçap giriniz : ");
+        int yaricap = scan.nextInt();
+
+        System.out.println("Girilen yarıçapın çevresi : "  + 2 * Math.PI * yaricap + "\nGirilen yarıçapın alanı : "  + (Math.PI * yaricap)*(Math.PI * yaricap) );
+    }
+}
