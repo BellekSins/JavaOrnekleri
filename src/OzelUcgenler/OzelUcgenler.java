1cebdaddc9d3980f0da4d5bf1c0caf1de7c4422c
Fatih Bellek <fatihbellek@outlook.com.tr>
2023-01-01 12:25:19 +0300

    OzelUcgenler created

---
+++ src/OzelUcgenler/OzelUcgenler.java
@@ -0,0 +1,39 @@
+package OzelUcgenler;
+
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
+public class OzelUcgenler {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+
+        int x ,z , y;
+        int i = 0;
+
+        while(i < 5){
+
+        x = random.nextInt(1,30);
+        y = random.nextInt(1,30);
+        z = random.nextInt(1,30);
+        
+            if (x * x + y * y == z * z ) {
+                System.out.println(x + "^2 + " + y + "^2 = " + z + "^2 ");
+            } else {
+              continue;
+            }
+
+            i++;
+            if (i == 5) {
+                System.out.println("Devam edeyim mi (y/n)");
+                String sonuc = scan.nextLine();
+                if (Objects.equals(sonuc, "y")) {
+                    i = 0;
+                }
+            }
+
+        }
+    }
+}
